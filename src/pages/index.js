828f60b336fe1f4fddde2a1b4850cb8cd9114d21
Мухаммад <human.twm@gmail.com>
2022-03-20 20:14:42 +0300

    Comment Contacts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,23 @@
 import * as React from "react";
 
-import Contacts from "../components/Contacts/index.js";
+// import Contacts from "../components/Contacts/index.js";
 import HeroPage from "../components/HeroPage/index.js";
 import "/src/styles/index.scss";
 
 const mainStyle = {
   height: "100%",
 }
-const contactsHidden = {
-  visibility: "hidden",
-  pointerEvents: "all",
-}
+// const contactsHidden = {
+//   visibility: "hidden",
+//   pointerEvents: "all",
+// }
 
 const IndexPage = () => {
   return (
     <main style={mainStyle}>
       <title>Mozy • Мобильные приложения</title>
       <HeroPage></HeroPage>
-      <Contacts style={contactsHidden}></Contacts>
+      {/* <Contacts style={contactsHidden}></Contacts> */}
     </main>
   );
 }
