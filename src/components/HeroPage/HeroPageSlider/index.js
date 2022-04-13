b5cc3cdc1bd628337667b64de8db587d233315c9
Мухаммад <human.twm@gmail.com>
2022-04-14 01:32:18 +0300

    Add lottie animation

--- src/components/HeroPage/HeroPageSlider/index.js
+++ src/components/HeroPage/HeroPageSlider/index.js
@@ -1,11 +1,21 @@
 import * as React from 'react';
+import Lottie from "react-lottie";
+import firstAnimation from "../../../lottie/first-animation.json";
 
 import {heroPageSlider} from "./style.module.scss";
 
 const HeroPageSlider = ({className}) => {
+    const animationOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: firstAnimation,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+    };
     return (
         <div className={`${className} ${heroPageSlider}`}>
-            <h1>mozy / animation</h1>
+            <Lottie options={animationOptions}></Lottie>
         </div>
     )
 };
