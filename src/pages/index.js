64de2ba1e4bc173797a54bdffc135b2b8495c7c0
Muhammad Temirsultanov <human.twm@gmail.com>
2023-04-01 13:36:04 +0300

    Refactoring: index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ import { throttle } from "../lib/utils";
 const MOBILE_HEIGHT = 700;
 const DELAY_BETWEEN_SLIDES = 1500;
 const TIME_TO_CHANGE_CLASSES = 500;
-
+const SECTIONS_LENGTH = 6;
 const sections = {
   promoBlock: 0,
   whatWeCanDo: 1,
@@ -53,17 +53,43 @@ function scrollTo(elem) {
   });
 }
 
-function onDocumentTouchtart(event) {
+function onDocumentTouchstart(event) {
   event.preventDefault();
+}
+
+function closeScrollToBody() {
+  document.body.classList.add("hidden");
+  document.addEventListener("touchstart", onDocumentTouchstart);
+}
+
+function openScrollToBody() {
+  document.body.classList.remove("hidden");
+  document.removeEventListener("touchstart", onDocumentTouchstart);
 }
 
 smoothscroll.polyfill();
 
 const IndexPage = () => {
-  const [opened, setOpened] = useState(sections.promoBlock);
-  const [closed, setClosed] = useState(sections.promoBlock);
+  const [openedSection, setOpenedSection] = useState(sections.promoBlock);
+  const [closedSection, setClosedSection] = useState(sections.promoBlock);
   const [contactsPopupClosed, setContactsPopupClosed] = useState(true);
   const [scrolling, setScrolling] = useState(false);
+  const portfolio = useRef(null);
+
+  const sectionsClassNames = useMemo(() => {
+    const classNames = Array(SECTIONS_LENGTH).fill("");
+    for (let i = 0; i < classNames.length; i++) {
+      if (openedSection === i) {
+        classNames[i] = "cur";
+        continue;
+      }
+
+      if (closedSection === i) classNames[i] = "prev ";
+      if (openedSection < i) classNames[i] = classNames[i] + "bottom";
+      if (openedSection > i) classNames[i] = classNames[i] + "top";
+    }
+    return classNames;
+  }, [openedSection, closedSection]);
 
   useEffect(() => {
     let onMouseWheel = function (event) {
@@ -72,12 +98,12 @@ const IndexPage = () => {
       if (scrolling === true) return;
 
       if (isScrollDown(event)) {
-        setOpened((cur) => {
+        setOpenedSection((cur) => {
           if (cur < sections.contactsAndFooter) return cur + 1;
           return cur;
         });
       } else if (isScrollUp(event)) {
-        setOpened((cur) => {
+        setOpenedSection((cur) => {
           if (cur > sections.promoBlock) return cur - 1;
           return cur;
         });
@@ -93,89 +119,34 @@ const IndexPage = () => {
 
   useLayoutEffect(() => {
     setTimeout(() => {
-      setClosed(opened);
+      setClosedSection(openedSection);
     }, DELAY_BETWEEN_SLIDES - TIME_TO_CHANGE_CLASSES);
-  }, [opened]);
-
-  const promoBlockClassName = useMemo(() => {
-    if (opened === sections.promoBlock) return "cur";
-
-    let res = "";
-    if (closed === sections.promoBlock) res = "prev ";
-    if (opened > sections.promoBlock) return res + "top";
-  }, [opened, closed]);
-
-  const whatWeCanDoClassName = useMemo(() => {
-    if (opened === sections.whatWeCanDo) return "cur";
-
-    let res = "";
-    if (closed === sections.whatWeCanDo) res = "prev ";
-    if (opened < sections.whatWeCanDo) return res + "bottom";
-    if (opened > sections.whatWeCanDo) return res + "top";
-  }, [opened, closed]);
-
-  const whyUsClassName = useMemo(() => {
-    if (opened === sections.whyUs) return "cur";
-
-    let res = "";
-    if (closed === sections.whyUs) res = "prev ";
-    if (opened < sections.whyUs) return res + "bottom";
-    if (opened > sections.whyUs) return res + "top";
-  }, [opened, closed]);
-
-  const portfolioClassName = useMemo(() => {
-    if (opened === sections.portfolio) return "cur";
-
-    let res = "";
-    if (closed === sections.portfolio) res = "prev ";
-    if (opened < sections.portfolio) return res + "bottom";
-    if (opened > sections.portfolio) return res + "top";
-  }, [opened, closed]);
-
-  const faqClassName = useMemo(() => {
-    if (opened === sections.faq) return "cur";
-
-    let res = "";
-    if (closed === sections.faq) res = "prev ";
-    if (opened < sections.faq) return res + "bottom";
-    if (opened === sections.contactsAndFooter) return res + "top";
-  }, [opened, closed]);
-
-  const contactsAndFooterClassName = useMemo(() => {
-    if (opened === sections.contactsAndFooter) return "cur";
-
-    let res = "";
-    if (closed === sections.contactsAndFooter) res = "prev ";
-    if (opened < sections.contactsAndFooter) return res + "bottom";
-  }, [opened, closed]);
-
-  const portfolio = useRef(null);
-  const contacts = useRef(null);
+  }, [openedSection]);
 
   function openPortfolio() {
-    setOpened(sections.portfolio);
+    if (window.innerWidth < MOBILE_HEIGHT) {
+      scrollTo(portfolio.current);
+      return;
+    }
+    
+    setOpenedSection(sections.portfolio);
     setScrolling(true);
     setTimeout(() => {
       setScrolling(false);
     }, DELAY_BETWEEN_SLIDES - TIME_TO_CHANGE_CLASSES);
-    if (window.innerWidth < MOBILE_HEIGHT) {
-      scrollTo(portfolio.current);
-    }
+    
   }
 
   function openContacts() {
     setContactsPopupClosed(false);
-    document.body.classList.add("hidden");
-    document.body.addEventListener("touchstart", onDocumentTouchtart);
+    closeScrollToBody();
   }
 
   function closeContacts() {
     setContactsPopupClosed(true);
-    document.body.classList.remove("hidden");
-    document.body.removeEventListener("touchstart", onDocumentTouchtart);
+    openScrollToBody()
+    
   }
-
-
 
   return (
     <Layout>
@@ -184,19 +155,16 @@ const IndexPage = () => {
         openPortfolio={openPortfolio}
         openBrief={openBrief}
         openContacts={openContacts}
-        className={promoBlockClassName}
+        className={sectionsClassNames[sections.promoBlock]}
       />
       <WhatWeCanDo
         openContacts={openContacts}
-        className={whatWeCanDoClassName}
+        className={sectionsClassNames[sections.whatWeCanDo]}
       />
-      <WhyUs openContacts={openContacts} className={whyUsClassName} />
-      <Portfolio ref={portfolio} className={portfolioClassName} />
-      <Faq className={faqClassName} />
-      <ContactsAndFooter
-        ref={contacts}
-        className={contactsAndFooterClassName}
-      />
+      <WhyUs openContacts={openContacts} className={sectionsClassNames[sections.whyUs]} />
+      <Portfolio ref={portfolio} className={sectionsClassNames[sections.portfolio]} />
+      <Faq className={sectionsClassNames[sections.faq]} />
+      <ContactsAndFooter className={sectionsClassNames[sections.contactsAndFooter]} />
     </Layout>
   );
 };
