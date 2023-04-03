cbbae99a22a4bd68721168a4bd8a3a9cbe3236da
Muhammad Temirsultanov <human.twm@gmail.com>
2023-04-03 23:42:50 +0300

    Feat: loop animation

--- src/components/PromoBlock/Animation/Animation.jsx
+++ src/components/PromoBlock/Animation/Animation.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState, useLayoutEffect } from "react";
 import "./style.scss";
 
 export const Animation = () => {
-    return (
-        <div className="animation">
-            <div className="animation__rectangle animation__rectangle-1"></div>
-            <div className="animation__rectangle animation__rectangle-2"></div>
-            <div className="animation__rectangle animation__rectangle-3"></div>
-            <div className="animation__rectangle animation__rectangle-4"></div>
-            <div className="animation__rectangle animation__rectangle-5"></div>
-            <div className="animation__rectangle animation__rectangle-6"></div>
-        </div>
-    )
+    const [animation, setAnimation] = useState(true);
+    useLayoutEffect(() => {
+        setInterval(() => {
+            setAnimation(false);
+        }, 10500)
+        setInterval(() => {
+            setAnimation(true);
+        }, 11000)
+    }, []);
+    return (animation ? <div className="animation animation-start">
+        <div className="animation__rectangle animation__rectangle-1"></div>
+        <div className="animation__rectangle animation__rectangle-2"></div>
+        <div className="animation__rectangle animation__rectangle-3"></div>
+        <div className="animation__rectangle animation__rectangle-4"></div>
+        <div className="animation__rectangle animation__rectangle-5"></div>
+        <div className="animation__rectangle animation__rectangle-6"></div>
+    </div> : null)
 }
