4bbe4043d1f872ef88511ecc4546ae33d0572f8d
Muhammad Temirsultanov <human.twm@gmail.com>
2023-04-06 15:03:05 +0300

    Add lottie animation

--- src/components/PromoBlock/Animation/Animation.jsx
+++ src/components/PromoBlock/Animation/Animation.jsx
@@ -1,17 +1,18 @@
-import React from "react";
-import Lottie from "react-lottie";
+import React, { useRef, useEffect } from "react";
+import Lottie from "lottie-react";
 import lottieAnimation from "../../../lottie/animation.json";
 
 export const Animation = () => {
-    const animationOptions = {
-        loop: true,
-        autoplay: true,
-        isClickToPauseDisabled: true,
-        rendererSettings: {
-            preserveAspectRatio: "xMidYMid slice"
-        },
-        animationData: lottieAnimation,
-    }
-
-    return <Lottie options={animationOptions} />
+    const wrapper = useRef(null);
+    const animationStyle = {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100%",
+    };
+    useEffect(() => {
+        wrapper.current.animationContainerRef.current.children[0].setAttribute("preserveAspectRatio", "xMidYMid slice");
+    }, []);
+    return <Lottie lottieRef={wrapper} animationData={lottieAnimation} loop={true} autoPlay={true} style={animationStyle} />
 }
